38a0b77d0e9fb2e75b9194ae6e87e0dbbadd348b
Dustin <rdhammack88@hotmail.com>
2019-09-05 03:52:11 -0500

    Added a feedCount property to track the total feed count, also added an addComment method to call when the input is submitted to add a comment to the top of the feed. As well added a trackById method to track each feed-list-item - (currently have not checked the tracking to verify proper tracking) - Everything working, but avatar image order not changing when new items are added.

--- UI-user-feed-ng/src/app/feed/feed-list/feed-list.component.ts
+++ UI-user-feed-ng/src/app/feed/feed-list/feed-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DataService } from 'src/app/shared/data.service';
 import { User } from 'src/app/shared/user';
 
@@ -8,8 +8,13 @@ import { User } from 'src/app/shared/user';
   styleUrls: ['./feed-list.component.scss']
 })
 export class FeedListComponent implements OnInit {
-
+  // @Output() addComment: EventEmitter<any> = new EventEmitter();
   feedList: Array<any>;
+  feedCount: number;
+  comment: string;
+  // user: any;
+  // id: number;
+  // user: User;
 
   constructor(private dataService: DataService) { }
 
@@ -20,11 +25,53 @@ export class FeedListComponent implements OnInit {
     //   console.log(this.feedList);
     // })
 
-    this.dataService.getFeedData().subscribe((data: User) => {
+    this.dataService.getFeedData().subscribe((data: User[]) => {
       // console.log(data.feed);
       this.feedList = [...data.feed];
+      this.feedCount = this.feedList.length;
       console.log(this.feedList);
+      // console.log(this.user);
+      console.log(this.feedList.map(data => data))
     })
   }
 
+  addNewComment() {
+    if(!this.comment) return;
+    let newComment = this.comment;
+    let currentTime = new Date();
+    let timestamp = currentTime.getTime().toString();
+    let currentTimeOffset = currentTime.getTimezoneOffset().toString();
+    let commentData = {
+      user: 'User 3',
+      value: newComment.trim(),
+      id: 3,
+      timestamp: timestamp,
+      timeZoneOffset: currentTimeOffset,
+      likes: 0
+    }
+
+    console.log(commentData);
+
+    // this.addComment.emit(comment);
+
+
+    // let newComment = this.comment.trim();
+    // let currentTime = new Date();
+    // let currentTimeOffset = currentTime.getTimezoneOffset().toString();
+    // let newCommentData: User = {
+    //   user: 'User 3',
+    //   value: newComment,
+    //   id: 3,
+    //   timestamp: currentTime.getTime().toString(),
+    //   timeZoneOffset: currentTimeOffset,
+    //   likes: 0
+    // }
+    this.feedList.unshift(commentData);
+    this.feedCount++;
+    this.comment = '';
+  }
+
+  trackById(index:number, item: User) {
+    return item.id;
+  }
 }
